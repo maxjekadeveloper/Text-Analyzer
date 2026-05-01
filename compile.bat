63520212f3baec31f2d105b530ba3e9cd394db06
Max Jeka <max.software.engineer.com>
2026-05-01 16:37:11 +0300

    .

--- compile.bat
+++ compile.bat
@@ -2,3 +2,6 @@ echo text_analyzer building...
 cmake -S . -B build -G Ninja -DCMAKE_BUILD_TYPE=Release
 cmake --build build
 echo complete!
+
+:: example
+:: ./text_analyzer "C:\Users\Max\Documents\final_project\" "in" 3
